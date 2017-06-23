7cbbb5a35d55248d3d052371176954562d71f182
jenny chen <jenny850214@gmail.com>
2017-06-23 16:08:35 +0800

    11103137126

---
+++ MVC_FinalExam/Controllers/CustomerController.cs
@@ -0,0 +1,115 @@
+﻿using MVC_FinalExam.ModelsAdd;
+using MVC_FinalExam.ModelsService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_FinalExam.Controllers
+{
+    public class CustomerController : Controller
+    {
+        CustomerService customerService = new CustomerService();
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region 查詢的部分
+        public ActionResult QueryCustomer()
+        {
+            DropDown_Title();
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult QueryCustomer(CustomerModel CusModel)
+        {
+            return this.Json(customerService.GetQueryResult(CusModel));
+        }
+        #endregion
+
+        #region 刪除的部分
+        [HttpPost]
+        public void QueryPage_Delete(int CustomerID)
+        {
+            customerService.DeleteQueryResult(CustomerID);
+        }
+        #endregion
+
+        #region 新增的部分
+        public ActionResult InsertCustomer()
+        {
+            DropDown_Title();
+            return View();
+        }
+
+        [HttpPost]
+        public void InsertCustomer(CustomerModel CusModel)
+        {
+            customerService.SaveCustomer(CusModel);
+        }
+        #endregion
+
+        #region 修改的部分
+        public ActionResult UpdateCustomer(int CustomerID)
+        {
+            DropDown_Title();
+            ViewBag.UpdateCustomerID = CustomerID;
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult UpdateCustomer_getData(int CustomerID)
+        {
+            return this.Json(customerService.GetUpdateData(CustomerID));
+        }
+
+
+        #endregion
+
+        #region MyViewBag
+        public void DropDown_Title()
+        {
+            ViewBag.CustomerTitle = GetSelectListItem(customerService.GetCustomreTitle().ToArray());
+        }
+
+
+        public List<SelectListItem> GetSelectListItem(String[] listStr)
+        {
+            List<SelectListItem> result = new List<SelectListItem>();
+            string value = "";
+            string[] MySplit = { };
+            for (int i = -1; i < listStr.Length; i++)
+            {
+                if (i != -1)
+                {
+                    MySplit = listStr[i].Split('_');
+                    value = MySplit[0];
+                }
+                else
+                {
+                    value = "0";
+                }
+
+                string text = (i == -1) ? text = string.Empty : text = MySplit[1];
+
+                result.Add(new SelectListItem
+                {
+                    Value = value,
+                    Text = text
+                });
+            }
+
+            return result;
+        }
+        #endregion
+
+
+
+    }
+}
---
+++ MVC_FinalExam/ModelsService/CustomerService.cs
@@ -0,0 +1,181 @@
+﻿using MVC_FinalExam.Models;
+using MVC_FinalExam.ModelsAdd;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_FinalExam.ModelsService
+{
+    public class CustomerService
+    {
+        
+        MyDB db = new MyDB();
+
+        /// <summary>
+        /// 取得員工職稱
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetCustomreTitle()
+        {
+            List<string> result = new List<string>();
+            result.Add("0001_Owner");
+            result.Add("0002_Order Administrator");
+            result.Add("0003_Sales Representative");
+            result.Add("0004_Sales Agent");
+            //var data = db.Customers
+            //         .Select(x => new
+            //         {
+            //             x.ContactTitle
+            //         }).ToList();
+
+            //CodeTable未有索引鍵，因而無法匯入!!
+            //因時間不夠，故先寫死
+
+            //int index = 0;
+            //foreach (var item in data)
+            //{
+            //    index++;
+            //    if (item.ContactTitle == "0001")
+            //    {
+            //        result.Add(index+"_"+"Owner");
+            //    }
+            //    else if (item.ContactTitle == "0002")
+            //    {
+            //        result.Add(index + "_" + "Order Administrator");
+            //    }
+            //    else if (item.ContactTitle == "0003")
+            //    {
+            //        result.Add(index + "_" + "Sales Representative");
+            //    }
+            //    else if (item.ContactTitle == "0004")
+            //    {
+            //        result.Add(index + "_" + "Sales Agent");
+            //    }
+            //}
+            return result;
+        }
+
+
+        /// <summary>
+        /// 取得查詢結果
+        /// </summary>
+        /// <returns></returns>
+        public List<CustomerModel> GetQueryResult(CustomerModel cusModel)
+        {
+            List<CustomerModel> result = new List<CustomerModel>();
+            var data = db.Customers
+                    .Select(x => new
+                    {
+                        x.CustomerID,
+                        x.CompanyName,
+                        x.ContactName,
+                        x.ContactTitle
+                    })
+                    .Where(x =>
+                            (cusModel.CustomerID == 0 || x.CustomerID.ToString().Contains(cusModel.CustomerID.ToString())) &&
+                            (cusModel.CompanyName == null || x.CompanyName.ToString().Contains(cusModel.CompanyName.ToString())) &&
+                            (cusModel.ContactName == null || x.ContactName.ToString().Contains(cusModel.ContactName.ToString())) &&
+                            (cusModel.ContactTitle == "0"|| x.ContactTitle == cusModel.ContactTitle)
+                     )
+                    .OrderBy(x => x.CustomerID)
+                    .ToList();
+
+
+            //CodeTable未有索引鍵，因而無法匯入!!
+            //因時間不夠，故先寫死
+            //Assistant 0003  ???????
+            string[] ptr = { "0001", "0002", "0003", "0004", "Assistant 0003" };
+            string[] codeVal = { "Owner" , "Order Administrator" , "Sales Representative", "Sales Agent" , "" };
+
+            foreach (var item in data)
+            {
+               
+                CustomerModel CustomerModel = new CustomerModel()
+                {
+                    CustomerID = item.CustomerID,
+                    CompanyName = item.CompanyName,
+                    ContactName = item.ContactName,
+                    ContactTitle = item.ContactTitle+"-"+codeVal[ptr.ToList().IndexOf(item.ContactTitle)]
+                };
+                result.Add(CustomerModel);
+            }
+
+            return result;
+        }
+
+
+
+        /// <summary>
+        /// 刪除一筆客戶
+        /// </summary>
+        /// <param name="id"></param>
+        public void DeleteQueryResult(int id)
+        {
+            var del_Cust = db.Customers
+                           .Where(x => x.CustomerID == id)
+                           .ToList();
+            db.Customers.RemoveRange(del_Cust);
+
+            db.SaveChanges();
+        }
+
+        /// <summary>
+        /// 儲存新增的客戶資料
+        /// </summary>
+        /// <param name="cusModel"></param>
+        public void SaveCustomer(CustomerModel cusModel)
+        {
+            Customers customer = new Customers()
+            {
+
+                CompanyName = cusModel.CompanyName,
+                ContactName = cusModel.ContactName,
+                ContactTitle = cusModel.ContactTitle,
+                CreationDate = Convert.ToDateTime(cusModel.CreationDate),
+                Address = cusModel.Address,
+                Region = cusModel.Region,
+                City = cusModel.City,
+                PostalCode = cusModel.PostalCode,
+                Country = cusModel.Country,
+                Phone = cusModel.Phone
+            };
+
+            db.Customers.Add(customer);
+            db.SaveChanges();
+
+        }
+
+
+
+        /// <summary>
+        /// 取得要修改的客戶資料
+        /// </summary>
+        /// <param name="CustomerID"></param>
+        /// <returns></returns>
+        public CustomerModel GetUpdateData(int CustomerID)
+        {
+            var data = db.Customers
+                       .Where(x => x.CustomerID == CustomerID)
+                       .First();
+
+            CustomerModel cu = new CustomerModel()
+            {
+                CustomerID = data.CustomerID,
+                CompanyName = data.CompanyName,
+                ContactName = data.ContactName,
+                ContactTitle = data.ContactTitle,
+                CreationDate = data.CreationDate.ToShortDateString(),
+                Address = data.Address,
+                Region = data.Region,
+                City = data.City,
+                PostalCode = data.PostalCode,
+                Country = data.Country,
+                Phone = data.Phone,
+                Fax = data.Fax
+            };
+
+            return cu;
+        }
+    }
+}
